4b21c1207f74e2c7f7e7d1d2614dfe432926f374
Matthew Powelson <matthew.powelson@swri.org>
2019-02-26 16:29:29 -0600

    Add TrajOptFreespacePlanner
    This planner is intended to provide an easy to use interface to TrajOpt for freespace planning. It is made to take a start and end point and automate the generation of the TrajOpt problem. It does not expose all of the configuration available in TrajOpt, but is made in hopes of lowering the barrier to entry for using TrajOpt to solve simple problems.

---
+++ tesseract_planning/include/tesseract_planning/trajopt/trajopt_freespace_planner.h
@@ -0,0 +1,144 @@
+/**
+ * @file trajopt_freespace_planner.h
+ * @brief Tesseract ROS TrajOpt Freespace planner
+ *
+ * @author Matthew Powelson
+ * @date February 26, 2019
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2019, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef TESSERACT_PLANNING_TRAJOPT_FREESPACE_PLANNER_H
+#define TESSERACT_PLANNING_TRAJOPT_FREESPACE_PLANNER_H
+
+#include <tesseract_core/macros.h>
+TESSERACT_IGNORE_WARNINGS_PUSH
+#include <trajopt/problem_description.hpp>
+TESSERACT_IGNORE_WARNINGS_POP
+
+#include <tesseract_planning/basic_planner.h>
+#include <tesseract_ros/ros_basic_plotting.h>
+#include <tesseract_planning/waypoint_definitions.h>
+
+namespace tesseract
+{
+namespace tesseract_planning
+{
+/**
+ * @brief Config to setup freespace planner. Specify the start and end position. Freespace motion can be defined between
+ * either joint space positions or cartesian positions
+ *
+ * While there are many parameters that can be set, the majority of them have defaults that will be suitable for many
+ * problems. These are always required: kin_, env_, end_effector
+ *
+ */
+struct TrajOptFreespacePlannerConfig
+{
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  TrajOptFreespacePlannerConfig() {}
+  virtual ~TrajOptFreespacePlannerConfig() {}
+
+  /** @brief Determines the constraint placed at the start of the trajectory */
+  WaypointPtr start_waypoint_;
+  /** @brief Determines the constraint placed at the end of the trajectory */
+  WaypointPtr end_waypoint_;
+
+  // TODO: These are waypoints the planner must hit in between
+  std::vector<Waypoint> intermediate_waypoints;
+
+  /** @brief The total number of timesteps used in the freespace motion. Default: 20 */
+  int num_steps_ = 20;
+
+  /** @brief If true, collision checking will be enabled. Default: true*/
+  bool collision_check_ = true;
+  /** @brief If true, use continuous collision checking */
+  bool collision_continuous_ = true;
+  /** @brief Max distance over which collisions are checked */
+  double collision_safety_margin_ = 0.025;
+  /** @brief If true, a joint velocity cost with a target of 0 will be applied for all timesteps Default: true*/
+  bool smooth_velocities_ = true;
+  /** @brief If true, a joint acceleration cost with a target of 0 will be applied for all timesteps Default: false*/
+  bool smooth_accelerations_ = true;
+  /** @brief If true, a joint jerk cost with a target of 0 will be applied for all timesteps Default: false*/
+  bool smooth_jerks_ = true;
+  /** @brief If true, add a callback to plot each iteration */
+  bool plot_callback_ = false;
+
+  /** @brief Basic Kinematics object. ***REQUIRED*** */
+  tesseract::BasicKinConstPtr kin_;
+  /** @brief Basic Environment object. ***REQUIRED*** */
+  tesseract::tesseract_ros::ROSBasicEnvConstPtr env_;
+  /** @brief This is the link used for the cartesian positions ***REQUIRED*** */
+  std::string end_effector_;
+  /** @brief Manipulator used for pathplanning ***REQUIRED*** */
+  std::string manipulator_;
+
+  /** @brief Optimization parameters to be used (Optional) */
+  sco::BasicTrustRegionSQPParameters params_;
+
+  /** @brief Callback functions called on each iteration of the optimization (Optional) */
+  std::vector<sco::Optimizer::Callback> callbacks_;
+};
+
+/**
+ * @brief This planner is intended to provide an easy to use interface to TrajOpt for freespace planning. It is made to
+ * take a start and end point and automate the generation of the TrajOpt problem.
+ */
+class TrajOptFreespacePlanner : public BasicPlanner
+{
+public:
+  /** @brief Construct a basic planner */
+  TrajOptFreespacePlanner()
+  {
+    name_ = "TRAJOPT_FREESPACE";
+
+    // Error Status Codes
+    status_code_map_[-1] = "Invalid config data format";
+    status_code_map_[-2] = "Failed to parse config data";
+    status_code_map_[-3] = "";
+
+    // Converge Status Codes
+  }
+
+  // TODO: Not sure what to do here, but this is defined in the base as virtual
+  bool solve(PlannerResponse& response) override { return false; };
+
+  /**
+   * @brief Sets up the TrajOpt problem then solves using TrajOptPlanner. It is intended to simplify setting up and
+   * solving freespace motion problems.
+   *
+   * This planner (and the associated config) does not expose all of the available configuration data in TrajOpt. This
+   * is done to simplify the interface. However, many problems may require more specific setups. In that case, the
+   * source code for this planner may be used as an example.
+   *
+   * Note: This does not use the request information because everything is provided by config parameter
+   *
+   * @param response The results of the optimization. Primary output is the optimized joint trajectory
+   * @param config Configures the freespace planner
+   * @return true if optimization complete
+   */
+  bool solve(PlannerResponse& response,  TrajOptFreespacePlannerConfig& config);
+
+  bool terminate() override;
+
+  void clear() override;
+};
+}  // namespace tesseract_planning
+}  // namespace tesseract
+#endif  // TESSERACT_PLANNING_TRAJOPT_PLANNER_H
---
+++ tesseract_planning/src/trajopt/trajopt_freespace_planner.cpp
@@ -0,0 +1,217 @@
+/**
+ * @file trajopt_planner.cpp
+ * @brief Tesseract ROS Trajopt planner
+ *
+ * @author Levi Armstrong
+ * @date April 18, 2018
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2017, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <tesseract_core/macros.h>
+TESSERACT_IGNORE_WARNINGS_PUSH
+#include <jsoncpp/json/json.h>
+#include <ros/console.h>
+#include <trajopt/plot_callback.hpp>
+#include <trajopt/problem_description.hpp>
+#include <trajopt_utils/config.hpp>
+#include <trajopt_utils/logging.hpp>
+#include <trajopt_sco/optimizers.hpp>
+#include <trajopt_sco/sco_common.hpp>
+TESSERACT_IGNORE_WARNINGS_POP
+
+#include <tesseract_planning/trajopt/trajopt_freespace_planner.h>
+#include <tesseract_planning/trajopt/trajopt_planner.h>
+
+// This is probably an issue
+#include <tesseract_ros/ros_basic_plotting.h>
+#include <trajopt/plot_callback.hpp>
+
+using namespace trajopt;
+
+namespace tesseract
+{
+namespace tesseract_planning
+{
+bool TrajOptFreespacePlanner::solve(PlannerResponse& response, TrajOptFreespacePlannerConfig& config)
+{
+  // Check that parameters are valid
+  if (config.env_ == nullptr)
+    throw std::invalid_argument("In trajopt_freespace_planner: env_ is a required parameter and has not been set");
+  if (config.kin_ == nullptr)
+    throw std::invalid_argument("In trajopt_freespace_planner: kin_ is a required parameter and has not been set");
+
+  // -------- Construct the problem ------------
+  // -------------------------------------------
+  ProblemConstructionInfo pci(config.env_);
+  pci.kin = config.kin_;
+
+  // Populate Basic Info
+  pci.basic_info.n_steps = config.num_steps_;
+  pci.basic_info.manip = manipulator_;
+  pci.basic_info.start_fixed = false;
+  pci.basic_info.use_time = false;
+
+  // Populate Init Info
+  pci.init_info.type = trajopt::InitInfo::STATIONARY;
+
+  // Set initial point
+  auto start_type = config.start_waypoint_->getType();
+  switch (start_type)
+  {
+    case tesseract::tesseract_planning::JOINT_WAYPOINT:
+    {
+      JointWaypointPtr start_position = std::static_pointer_cast<JointWaypoint>(config.start_waypoint_);
+      // Add initial joint position constraint
+      std::shared_ptr<JointPosTermInfo> jv = std::shared_ptr<JointPosTermInfo>(new JointPosTermInfo);
+      jv->coeffs = std::vector<double>(pci.kin->numJoints(), 1.0);
+      jv->targets = start_position->joint_positions_;
+      jv->first_step = 0;
+      jv->last_step = 0;
+      jv->name = "initial_joint_position";
+      jv->term_type = TT_CNT;
+      pci.cnt_infos.push_back(jv);
+      break;
+    }
+    case tesseract::tesseract_planning::CARTESIAN_WAYPOINT:
+    {
+      CartesianWaypointPtr start_pose = std::static_pointer_cast<CartesianWaypoint>(config.start_waypoint_);
+      std::shared_ptr<CartPoseTermInfo> pose = std::shared_ptr<CartPoseTermInfo>(new CartPoseTermInfo);
+      pose->term_type = TT_CNT;
+      pose->name = "initial_cartesian_position";
+      pose->link = config.end_effector_;
+      pose->timestep = 0;
+      pose->xyz = start_pose->getPosition();
+      pose->wxyz = start_pose->getOrientation();
+      pose->pos_coeffs = Eigen::Vector3d(10, 10, 10);
+      pose->rot_coeffs = Eigen::Vector3d(10, 10, 10);
+      pci.cnt_infos.push_back(pose);
+      break;
+    }
+  }
+
+  // Set final point
+  auto end_type = config.end_waypoint_->getType();
+  switch (end_type)
+  {
+    case tesseract::tesseract_planning::JOINT_WAYPOINT:
+    {
+      JointWaypointPtr end_position = std::static_pointer_cast<JointWaypoint>(config.end_waypoint_);
+      // Add initial joint position constraint
+      std::shared_ptr<JointPosTermInfo> jv = std::shared_ptr<JointPosTermInfo>(new JointPosTermInfo);
+      jv->coeffs = std::vector<double>(pci.kin->numJoints(), 1.0);
+      jv->targets = end_position->joint_positions_;
+      jv->first_step = pci.basic_info.n_steps - 1;
+      jv->last_step = pci.basic_info.n_steps - 1;
+      jv->name = "target_joint_position";
+      jv->term_type = TT_CNT;
+      pci.cnt_infos.push_back(jv);
+    }
+    case tesseract::tesseract_planning::CARTESIAN_WAYPOINT:
+    {
+      CartesianWaypointPtr end_pose = std::static_pointer_cast<CartesianWaypoint>(config.end_waypoint_);
+      std::shared_ptr<CartPoseTermInfo> pose = std::shared_ptr<CartPoseTermInfo>(new CartPoseTermInfo);
+      pose->term_type = TT_CNT;
+      pose->name = "target_cartesian_position";
+      pose->link = config.end_effector_;
+      pose->timestep = pci.basic_info.n_steps - 1;
+      pose->xyz = end_pose->getPosition();
+      pose->wxyz = end_pose->getOrientation();
+      pose->pos_coeffs = Eigen::Vector3d(10, 10, 10);
+      pose->rot_coeffs = Eigen::Vector3d(10, 10, 10);
+      pci.cnt_infos.push_back(pose);
+    }
+  }
+
+  // Set costs for the rest of the points
+  if (config.collision_check_)
+  {
+    std::shared_ptr<CollisionTermInfo> collision = std::shared_ptr<CollisionTermInfo>(new CollisionTermInfo);
+    collision->name = "collision_cost";
+    collision->term_type = TT_COST;
+    collision->continuous = config.collision_continuous_;
+    collision->first_step = 0;
+    collision->last_step = pci.basic_info.n_steps - 1;
+    collision->gap = 1;
+    collision->info = createSafetyMarginDataVector(pci.basic_info.n_steps, config.collision_safety_margin_, 20);
+    pci.cost_infos.push_back(collision);
+  }
+  if (config.smooth_velocities_)
+  {
+    std::shared_ptr<JointVelTermInfo> jv = std::shared_ptr<JointVelTermInfo>(new JointVelTermInfo);
+    jv->coeffs = std::vector<double>(pci.kin->numJoints(), 5.0);
+    jv->targets = std::vector<double>(pci.kin->numJoints(), 0.0);
+    jv->first_step = 0;
+    jv->last_step = pci.basic_info.n_steps - 1;
+    jv->name = "joint_velocity_cost";
+    jv->term_type = TT_COST;
+    pci.cost_infos.push_back(jv);
+  }
+  if (config.smooth_accelerations_)
+  {
+    std::shared_ptr<JointAccTermInfo> ja = std::shared_ptr<JointAccTermInfo>(new JointAccTermInfo);
+    ja->coeffs = std::vector<double>(pci.kin->numJoints(), 1.0);
+    ja->targets = std::vector<double>(pci.kin->numJoints(), 0.0);
+    ja->first_step = 0;
+    ja->last_step = pci.basic_info.n_steps - 1;
+    ja->name = "joint_accel_cost";
+    ja->term_type = TT_COST;
+    pci.cost_infos.push_back(ja);
+  }
+  if (config.smooth_jerks_)
+  {
+    std::shared_ptr<JointJerkTermInfo> jj = std::shared_ptr<JointJerkTermInfo>(new JointJerkTermInfo);
+    jj->coeffs = std::vector<double>(pci.kin->numJoints(), 1.0);
+    jj->targets = std::vector<double>(pci.kin->numJoints(), 0.0);
+    jj->first_step = 0;
+    jj->last_step = pci.basic_info.n_steps - 1;
+    jj->name = "joint_jerk_cost";
+    jj->term_type = TT_COST;
+    pci.cost_infos.push_back(jj);
+  }
+  trajopt::TrajOptProbPtr prob = ConstructProblem(pci);
+
+  // -------- Solve the problem ------------
+  // ---------------------------------------
+  // Set the parameters in trajopt_planner
+  tesseract::tesseract_planning::TrajOptPlannerConfig config_planner(prob);
+  config_planner.params = config.params_;
+  config_planner.callbacks = config.callbacks_;
+
+  // Create Plot Callback
+  if (config.plot_callback_)
+  {
+    tesseract::tesseract_ros::ROSBasicPlottingPtr plotter_ptr(
+        new tesseract::tesseract_ros::ROSBasicPlotting(config.env_));
+    config_planner.callbacks.push_back(PlotCallback(*prob, plotter_ptr));
+  }
+
+  tesseract::tesseract_planning::TrajOptPlanner planner;
+  tesseract::tesseract_planning::PlannerResponse planning_response;
+
+  // Solve problem. Results are stored in the response
+  bool success = planner.solve(planning_response, config_planner);
+  response = planning_response;
+
+  return success;
+}
+
+bool TrajOptFreespacePlanner::terminate() { return false; }
+void TrajOptFreespacePlanner::clear() { request_ = PlannerRequest(); }
+}  // namespace tesseract_planning
+}  // namespace tesseract
