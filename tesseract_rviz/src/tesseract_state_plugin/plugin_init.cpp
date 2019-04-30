13562b74e34cacda6d3ca0117861c5ced9561630
Levi Armstrong <levi.armstrong@gmail.com>
2019-04-30 17:03:59 -0500

    WIP: Move ROS package into sub folder

--- tesseract_rviz/src/tesseract_state_plugin/plugin_init.cpp
+++
@@ -1,8 +0,0 @@
-#include <tesseract_core/macros.h>
-TESSERACT_IGNORE_WARNINGS_PUSH
-#include <pluginlib/class_list_macros.h>
-TESSERACT_IGNORE_WARNINGS_POP
-
-#include <tesseract_rviz/tesseract_state_plugin/tesseract_state_display.h>
-
-PLUGINLIB_EXPORT_CLASS(tesseract_rviz::TesseractStateDisplay, rviz::Display)
