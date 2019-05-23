c2f0a03a5a08d5fb014c163d5da4a52c1575c343
Levi Armstrong <levi.armstrong@gmail.com>
2019-05-22 23:28:02 -0500

    Add scale meta data to meshes loaded from file

--- tesseract_core/tesseract_geometry/include/tesseract_geometry/impl/convex_mesh.h
+++ tesseract_core/tesseract_geometry/include/tesseract_geometry/impl/convex_mesh.h
@@ -46,7 +46,7 @@ namespace tesseract_geometry
   public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-    ConvexMesh(const std::shared_ptr<const VectorVector3d>& vertices, const std::shared_ptr<const Eigen::VectorXi>& faces, std::string file_path = "") : Geometry(GeometryType::CONVEX_MESH), vertices_(vertices), faces_(faces), file_path_(file_path)
+    ConvexMesh(const std::shared_ptr<const VectorVector3d>& vertices, const std::shared_ptr<const Eigen::VectorXi>& faces, std::string file_path = "", Eigen::Vector3d scale = Eigen::Vector3d(1,1,1)) : Geometry(GeometryType::CONVEX_MESH), vertices_(vertices), faces_(faces), file_path_(file_path), scale_(scale)
     {
       vertice_count_ = static_cast<int>(vertices->size());
 
@@ -60,7 +60,7 @@ namespace tesseract_geometry
 
     }
 
-    ConvexMesh(const std::shared_ptr<const VectorVector3d>& vertices, const std::shared_ptr<const Eigen::VectorXi>& faces, int face_count, std::string file_path = "") : Geometry(GeometryType::CONVEX_MESH), vertices_(vertices), faces_(faces), face_count_(face_count), file_path_(file_path)
+    ConvexMesh(const std::shared_ptr<const VectorVector3d>& vertices, const std::shared_ptr<const Eigen::VectorXi>& faces, int face_count, std::string file_path = "", Eigen::Vector3d scale = Eigen::Vector3d(1,1,1)) : Geometry(GeometryType::CONVEX_MESH), vertices_(vertices), faces_(faces), face_count_(face_count), file_path_(file_path), scale_(scale)
     {
       vertice_count_ = static_cast<int>(vertices->size());
     }
@@ -82,7 +82,13 @@ namespace tesseract_geometry
      */
     const std::string& getFilePath() const { return file_path_; }
 
-    GeometryPtr clone() const override { return ConvexMeshPtr(new ConvexMesh(vertices_, faces_, face_count_, file_path_)); }
+    /**
+     * @brief Get the scale applied to file used to generate the mesh
+     * @return The scale x, y, z
+     */
+    const Eigen::Vector3d& getScale() const { return scale_; }
+
+    GeometryPtr clone() const override { return ConvexMeshPtr(new ConvexMesh(vertices_, faces_, face_count_, file_path_, scale_)); }
 
   private:
     std::shared_ptr<const VectorVector3d> vertices_;
@@ -91,6 +97,7 @@ namespace tesseract_geometry
     int vertice_count_;
     int face_count_;
     std::string file_path_;
+    Eigen::Vector3d scale_;
   };
 }
 #endif
