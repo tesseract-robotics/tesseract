749746bc42577dd4b7fc875b25aa9243456a3673
Matthew Powelson <matthew.powelson@swri.org>
2019-06-10 15:59:13 -0500

    Fix spelling in EnvironmentCommand.msg

--- tesseract_ros/tesseract_rosutils/include/tesseract_rosutils/utils.h
+++ tesseract_ros/tesseract_rosutils/include/tesseract_rosutils/utils.h
@@ -998,8 +998,8 @@ static inline bool toMsg(tesseract_msgs::EnvironmentCommand& command_msg, const 
     {
       command_msg.command = tesseract_msgs::EnvironmentCommand::CHANGE_JOINT_ORIGIN;
       const tesseract_environment::ChangeJointOriginCommand& cmd = static_cast<const tesseract_environment::ChangeJointOriginCommand&>(command);
-      command_msg.change_joint_orgin_name = cmd.getJointName();
-      tf::poseEigenToMsg(cmd.getOrigin(), command_msg.change_joint_orgin_pose);
+      command_msg.change_joint_origin_name = cmd.getJointName();
+      tf::poseEigenToMsg(cmd.getOrigin(), command_msg.change_joint_origin_pose);
       return true;
     }
     case tesseract_environment::CommandType::CHANGE_LINK_COLLISION_ENABLED:
@@ -1249,8 +1249,8 @@ static inline bool processMsg(tesseract_environment::Environment& env, const std
       case tesseract_msgs::EnvironmentCommand::CHANGE_JOINT_ORIGIN:
       {
         Eigen::Isometry3d pose;
-        tf::poseMsgToEigen(command.change_joint_orgin_pose, pose);
-        return env.changeJointOrigin(command.change_joint_orgin_name, pose);
+        tf::poseMsgToEigen(command.change_joint_origin_pose, pose);
+        return env.changeJointOrigin(command.change_joint_origin_name, pose);
       }
       case tesseract_msgs::EnvironmentCommand::CHANGE_LINK_COLLISION_ENABLED:
       {
