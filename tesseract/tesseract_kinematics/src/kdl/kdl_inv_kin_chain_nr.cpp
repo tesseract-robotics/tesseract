2477a3224e24964c89d75faf1c4510baec05cff1
Levi Armstrong <levi.armstrong@gmail.com>
2019-10-10 00:43:38 -0500

    Switch joint limit check from warning to debug log

--- tesseract/tesseract_kinematics/src/kdl/kdl_inv_kin_chain_nr.cpp
+++ tesseract/tesseract_kinematics/src/kdl/kdl_inv_kin_chain_nr.cpp
@@ -121,11 +121,11 @@ bool KDLInvKinChainNR::checkJoints(const Eigen::Ref<const Eigen::VectorXd>& vec)
   {
     if ((vec[i] < kdl_data_.joint_limits(i, 0)) || (vec(i) > kdl_data_.joint_limits(i, 1)))
     {
-      CONSOLE_BRIDGE_logWarn("Joint %s is out-of-range (%g < %g < %g)",
-                             kdl_data_.joint_list[static_cast<size_t>(i)].c_str(),
-                             kdl_data_.joint_limits(i, 0),
-                             vec(i),
-                             kdl_data_.joint_limits(i, 1));
+      CONSOLE_BRIDGE_logDebug("Joint %s is out-of-range (%g < %g < %g)",
+                              kdl_data_.joint_list[static_cast<size_t>(i)].c_str(),
+                              kdl_data_.joint_limits(i, 0),
+                              vec(i),
+                              kdl_data_.joint_limits(i, 1));
     }
   }
 
