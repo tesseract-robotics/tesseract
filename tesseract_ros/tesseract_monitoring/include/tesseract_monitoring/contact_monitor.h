19bc5c8faa9bbf4bfa1b37ae449da2827afbacc1
dmerz <david.merz@swri.org>
2020-02-17 14:22:58 -0600

    Editing license comments

--- tesseract_ros/tesseract_monitoring/include/tesseract_monitoring/contact_monitor.h
+++ tesseract_ros/tesseract_monitoring/include/tesseract_monitoring/contact_monitor.h
@@ -1,18 +1,30 @@
-/*
- * Copyright 2020 Southwest Research Institute
+/**
+ * @file contact_monitor.h
+ * @brief definition of the contact_monitor library.  It publishes
+ * info about which links are (almost) in collision, and how far from/in
+ * collision they are.
  *
+ * @author David Merz, Jr.
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2020, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #ifndef CONTACT_MONITOR_H
 #define CONTACT_MONITOR_H
 
