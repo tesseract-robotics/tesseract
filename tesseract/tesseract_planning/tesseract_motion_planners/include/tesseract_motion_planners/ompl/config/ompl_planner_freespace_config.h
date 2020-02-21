bb8759d38cff027622e018225eccca9cd3432ed9
Joseph Schornak <joe.schornak@gmail.com>
2020-02-21 14:44:15 -0600

    comment out unused parameter

--- tesseract/tesseract_planning/tesseract_motion_planners/include/tesseract_motion_planners/ompl/config/ompl_planner_freespace_config.h
+++ tesseract/tesseract_planning/tesseract_motion_planners/include/tesseract_motion_planners/ompl/config/ompl_planner_freespace_config.h
@@ -79,11 +79,10 @@ struct OMPLPlannerFreespaceConfig : public OMPLPlannerConfig
       std::bind(&OMPLPlannerFreespaceConfig::allocWeightedRealVectorStateSampler, this, std::placeholders::_1);
 
   /** @brief Set the optimization objective function allocator. Default is to minimize path length */
-  OptimizationObjectiveAllocator optimization_objective_allocator =
-      [](const ompl::base::SpaceInformationPtr& si,
-         const OMPLPlannerConfig& config) {
-        return std::make_shared<ompl::base::PathLengthOptimizationObjective>(si);
-      };
+  OptimizationObjectiveAllocator optimization_objective_allocator = [](const ompl::base::SpaceInformationPtr& si,
+                                                                       const OMPLPlannerConfig& /*config*/) {
+    return std::make_shared<ompl::base::PathLengthOptimizationObjective>(si);
+  };
 
   /** @brief The ompl state validity checker. If nullptr it uses OMPLFreespacePlanner::isStateValid. */
   ompl::base::StateValidityCheckerFn svc;
