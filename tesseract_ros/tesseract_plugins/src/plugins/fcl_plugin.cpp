4302da568007287c0123ecbea711de73f4effcc2
mpowelson <matthew.powelson@swri.org>
2020-03-05 15:43:08 -0600

    Remove tesseract_ros
    This will be moved to an independent repository

--- tesseract_ros/tesseract_plugins/src/plugins/fcl_plugin.cpp
+++
@@ -1,9 +0,0 @@
-#include <tesseract_common/macros.h>
-TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
-#include <class_loader/class_loader.hpp>
-TESSERACT_COMMON_IGNORE_WARNINGS_POP
-
-#include <tesseract_collision/fcl/fcl_discrete_managers.h>
-
-CLASS_LOADER_REGISTER_CLASS(tesseract_collision::tesseract_collision_fcl::FCLDiscreteBVHManager,
-                            tesseract_collision::DiscreteContactManager)
--- tesseract_ros/tesseract_rviz/src/tesseract_state_plugin/plugin_init.cpp
+++
@@ -1,8 +0,0 @@
-#include <tesseract_common/macros.h>
-TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
-#include <class_loader/class_loader.hpp>
-TESSERACT_COMMON_IGNORE_WARNINGS_POP
-
-#include <tesseract_rviz/tesseract_state_plugin/tesseract_state_display.h>
-
-CLASS_LOADER_REGISTER_CLASS(tesseract_rviz::TesseractStateDisplay, rviz::Display)
