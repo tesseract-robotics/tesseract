7aaae42b5fdc78c6a0680913ec20d393fb7fdaa5
mpowelson <matthew.powelson@swri.org>
2020-03-05 15:43:08 -0600

    Remove tesseract_ros
    This will be moved to an independent repository

--- tesseract_ros/tesseract_rviz/src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "tesseract_rviz/tesseract_trajectory_plugin/tesseract_trajectory_display.h"
-#include "tesseract_rviz/tesseract_state_plugin/tesseract_state_display.h"
-int main(int /*argc*/, char** /*argv[]*/)
-{
-  tesseract_rviz::TesseractTrajectoryDisplay test_trajectory_display;
-  tesseract_rviz::TesseractStateDisplay test_state_display;
-}
