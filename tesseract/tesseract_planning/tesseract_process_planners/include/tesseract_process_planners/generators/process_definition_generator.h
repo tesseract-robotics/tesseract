bb10d30a9fecd35b7aa69daea51ca242dfb9e9c6
ctlewis <colin.lewis@swri.org>
2020-05-11 17:23:16 -0500

    Created Doxygen, removed constructor, and standardized compiler def

--- tesseract/tesseract_planning/tesseract_process_planners/include/tesseract_process_planners/generators/process_definition_generator.h
+++ tesseract/tesseract_planning/tesseract_process_planners/include/tesseract_process_planners/generators/process_definition_generator.h
@@ -1,5 +1,5 @@
-#ifndef PROCESS_DEFINITION_GENERATOR_H
-#define PROCESS_DEFINITION_GENERATOR_H
+#ifndef TESSERACT_PROCESS_PLANNERS_PROCESS_DEFINITION_GENERATOR_H
+#define TESSERACT_PROCESS_PLANNERS_PROCESS_DEFINITION_GENERATOR_H
 
 #include <tesseract_process_planners/process_definition.h>
 #include <tesseract_motion_planners/core/waypoint.h>
@@ -17,16 +17,26 @@ public:
   using ToolPath = std::vector<tesseract_motion_planners::Waypoint::Ptr>;
   using ToolPaths = std::vector<ToolPath>;
 
-  ProcessDefinitionGenerator() = default;
   virtual ~ProcessDefinitionGenerator() = default;
 
+  /**
+   * @brief generate Generates a process definition from a toolpath
+   * @param start The intial robot position
+   * @param end  The final robot position
+   * @param tool_paths The vector of toolpath rasters to be processed
+   * @return A process definition encompassing the segments and transitions of the process
+   */
   virtual ProcessDefinition generate(tesseract_motion_planners::Waypoint::ConstPtr start,
                                      tesseract_motion_planners::Waypoint::ConstPtr end,
                                      const ToolPaths& tool_paths) const = 0;
-
+  /**
+   * @brief generate Generates a process definition from a toolpath
+   * @param tool_paths The vector of toolpath rasters to be processed
+   * @return A process definition encompassing the segments and transitions of the process
+   */
   virtual ProcessDefinition generate(const ToolPaths& tool_paths) const = 0;
 };
 
 }  // namespace tesseract_process_planners
 
-#endif  // PROCESS_DEFINITION_GENERATOR_H
+#endif  //TESSERACT_PROCESS_PLANNERS_PROCESS_DEFINITION_GENERATOR_H
