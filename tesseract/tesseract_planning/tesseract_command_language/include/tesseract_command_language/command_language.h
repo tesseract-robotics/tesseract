13847690c0e5d380d9269df168749d1acef846c6
Matthew Powelson <powelson.matthew@gmail.com>
2020-07-07 16:03:38 -0500

    Add command_language.h

---
+++ tesseract/tesseract_planning/tesseract_command_language/include/tesseract_command_language/command_language.h
@@ -0,0 +1,12 @@
+#ifndef TESSERACT_COMMAND_LANGUAGE_COMMAND_LANGUAGE_H
+#define TESSERACT_COMMAND_LANGUAGE_COMMAND_LANGUAGE_H
+
+#include <tesseract_command_language/cartesian_waypoint.h>
+#include <tesseract_command_language/composite_instruction.h>
+#include <tesseract_command_language/instruction_type.h>
+#include <tesseract_command_language/joint_waypoint.h>
+#include <tesseract_command_language/move_instruction.h>
+#include <tesseract_command_language/plan_instruction.h>
+#include <tesseract_command_language/waypoint_type.h>
+
+#endif  // TESSERACT_COMMAND_LANGUAGE_WAYPOINT_TYPE_H
