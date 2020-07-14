88d1a8374aa5b74f621d57074e7de974d2281434
Matthew Powelson <powelson.matthew@gmail.com>
2020-07-14 15:53:39 -0500

    Move Flatten Utilities into tesseract_command_language

--- tesseract/tesseract_planning/tesseract_command_language/include/tesseract_command_language/command_language_utils.h
+++ tesseract/tesseract_planning/tesseract_command_language/include/tesseract_command_language/command_language_utils.h
@@ -95,6 +95,73 @@ long getMoveInstructionsCount(const CompositeInstruction& composite_instruction,
  */
 long getPlanInstructionsCount(const CompositeInstruction& composite_instruction, bool process_child_composites = true);
 
+/**
+ * @brief Helper function used by Flatten. Not intended for direct use
+ * @param flattened Vector of instructions representing the full flattened composite
+ * @param composite Composite instruction to be flattened
+ * @param include_composite If true, CompositeInstructions will be included in the final flattened vector
+ */
+void flattenHelper(std::vector<std::reference_wrapper<Instruction>>& flattened,
+                   CompositeInstruction& composite,
+                   const bool& include_composite);
+
+/**
+ * @brief Flattens a CompositeInstruction into a vector of Instruction&
+ * @param instruction Input composite instruction to be flattened
+ * @param include_composite Default: false. If true, CompositeInstructions will be included in the final flattened
+ * vector
+ * @return A new flattened vector referencing the original instruction elements
+ */
+std::vector<std::reference_wrapper<Instruction>> flatten(CompositeInstruction& instruction,
+                                                         bool include_composite = false);
+/**
+ * @brief Helper function used by Flatten. Not intended for direct use
+ * @param flattened Vector of instructions representing the full flattened composite
+ * @param composite Composite instruction to be flattened
+ * @param include_composite If true, CompositeInstructions will be included in the final flattened vector
+ */
+void flattenHelper(std::vector<std::reference_wrapper<const Instruction>>& flattened,
+                   const CompositeInstruction& composite,
+                   const bool& include_composite);
+
+/**
+ * @brief Flattens a CompositeInstruction into a vector of Instruction&
+ * @param instruction Input composite instruction to be flattened
+ * @param include_composite Default: false. If true, CompositeInstructions will be included in the final flattened
+ * vector
+ * @return A new flattened vector referencing the original instruction elements
+ */
+std::vector<std::reference_wrapper<const Instruction>> flatten(const CompositeInstruction& instruction,
+                                                               bool include_composite = false);
+
+/**
+ * @brief Helper function used by FlattenToPattern. Not intended for direct use
+ * @param flattened Vector of instructions representing the full flattened composite
+ * @param composite Composite instruction to be flattened
+ * @param pattern CompositeInstruction used to determine if instruction will be flattened
+ * @param include_composite If true, CompositeInstructions will be included in the final flattened vector
+ */
+void flattenToPatternHelper(std::vector<std::reference_wrapper<Instruction>>& flattened,
+                            CompositeInstruction& composite,
+                            const CompositeInstruction& pattern,
+                            const bool& include_composite);
+
+/**
+ * @brief Flattens a composite instruction to the same pattern as the pattern composite instruction. ie, an element of
+ * instruction will only be flattened if the corresponding element in pattern is flattenable.
+ *
+ * The motivation for this utility is a case where you flatten only the elements in a seed that correspond to composites
+ * in the parent instruction
+ * @param instruction CompositeInstruction that will be flattened
+ * @param pattern CompositeInstruction used to determine if instruction will be flattened
+ * @param include_composite Default: false. If true, CompositeInstructions will be included in the final flattened
+ * vector
+ * @return A new flattened vector referencing the original instruction elements
+ */
+std::vector<std::reference_wrapper<Instruction>> flattenToPattern(CompositeInstruction& instruction,
+                                                                  const CompositeInstruction& pattern,
+                                                                  bool include_composite = false);
+
 }  // namespace tesseract_planning
 
 #endif  // TESSERACT_COMMAND_LANGUAGE_COMMAND_LANGUAGE_UTILS_H
