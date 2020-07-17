bd648528f6cc8cd685bebe129357fdd1d6645f53
Matthew Powelson <powelson.matthew@gmail.com>
2020-07-17 16:51:23 -0500

    Update start and end Instructions in process managers

--- tesseract/tesseract_planning/tesseract_process_managers/src/process_generators/trajopt_process_generator.cpp
+++ tesseract/tesseract_planning/tesseract_process_managers/src/process_generators/trajopt_process_generator.cpp
@@ -1,4 +1,5 @@
-﻿#include <tesseract_process_managers/process_generators/trajopt_process_generator.h>
+﻿#include <tesseract_command_language/command_language_utils.h>
+#include <tesseract_process_managers/process_generators/trajopt_process_generator.h>
 #include <tesseract_motion_planners/trajopt/trajopt_motion_planner.h>
 
 #include <tesseract_motion_planners/trajopt/trajopt_motion_planner.h>
@@ -20,17 +21,55 @@ std::function<void()> TrajOptProcessGenerator::generateTask(ProcessInput input)
 {
   task_inputs_.push_back(input);
 
-  return std::bind(&TrajOptProcessGenerator::process, this, task_inputs_.back());
+  return std::bind(&TrajOptProcessGenerator::process, this, task_inputs_.back(), null_instruction, null_instruction);
+}
+
+std::function<void()> TrajOptProcessGenerator::generateTask(ProcessInput input, const Instruction& start_instruction)
+{
+  task_inputs_.push_back(input);
+
+  return std::bind(&TrajOptProcessGenerator::process, this, task_inputs_.back(), start_instruction, null_instruction);
+}
+
+std::function<void()> TrajOptProcessGenerator::generateTask(ProcessInput input,
+                                                            const Instruction& start_instruction,
+                                                            const Instruction& end_instruction)
+{
+  task_inputs_.push_back(input);
+
+  return std::bind(&TrajOptProcessGenerator::process, this, task_inputs_.back(), start_instruction, end_instruction);
 }
 
 std::function<int()> TrajOptProcessGenerator::generateConditionalTask(ProcessInput input)
 {
   task_inputs_.push_back(input);
 
-  return std::bind(&TrajOptProcessGenerator::conditionalProcess, this, task_inputs_.back());
+  return std::bind(
+      &TrajOptProcessGenerator::conditionalProcess, this, task_inputs_.back(), null_instruction, null_instruction);
 }
 
-int TrajOptProcessGenerator::conditionalProcess(ProcessInput input) const
+std::function<int()> TrajOptProcessGenerator::generateConditionalTask(ProcessInput input,
+                                                                      const Instruction& start_instruction)
+{
+  task_inputs_.push_back(input);
+
+  return std::bind(
+      &TrajOptProcessGenerator::conditionalProcess, this, task_inputs_.back(), start_instruction, null_instruction);
+}
+
+std::function<int()> TrajOptProcessGenerator::generateConditionalTask(ProcessInput input,
+                                                                      const Instruction& start_instruction,
+                                                                      const Instruction& end_instruction)
+{
+  task_inputs_.push_back(input);
+
+  return std::bind(
+      &TrajOptProcessGenerator::conditionalProcess, this, task_inputs_.back(), start_instruction, end_instruction);
+}
+
+int TrajOptProcessGenerator::conditionalProcess(const ProcessInput& input,
+                                                const Instruction& start_instruction,
+                                                const Instruction& end_instruction) const
 {
   // --------------------
   // Check that inputs are valid
@@ -46,6 +85,22 @@ int TrajOptProcessGenerator::conditionalProcess(ProcessInput input) const
     return 0;
   }
 
+  // Make a non-const copy of the input instructions to update the start/end
+  CompositeInstruction instructions = *input.instruction.cast_const<CompositeInstruction>();
+
+  // If the start and end waypoints need to be updated prior to planning
+  if (!isNullInstruction(start_instruction))
+  {
+    // add start
+    instructions.setStartInstruction(start_instruction);
+  }
+  if (!isNullInstruction(end_instruction))
+  {
+    // add end
+    assert(isMoveInstruction(end_instruction));
+    getLastPlanInstruction(instructions)->setWaypoint(end_instruction.cast_const<MoveInstruction>()->getWaypoint());
+  }
+
   // --------------------
   // Fill out request
   // --------------------
@@ -53,9 +108,9 @@ int TrajOptProcessGenerator::conditionalProcess(ProcessInput input) const
   request.seed = *input.results.cast<CompositeInstruction>();
   request.env_state = input.tesseract->getEnvironmentConst()->getCurrentState();
   request.tesseract = input.tesseract;
-  request.manipulator = "manipulator";  // TODO
+  request.manipulator = "manipulator";  // TODO: Where do I get this information?
   //  request.manipulator_ik_solver;
-  request.instructions = *input.instruction.cast_const<CompositeInstruction>();
+  request.instructions = instructions;
 
   // --------------------
   // Fill out response
@@ -81,6 +136,11 @@ int TrajOptProcessGenerator::conditionalProcess(ProcessInput input) const
   return 0;
 }
 
-void TrajOptProcessGenerator::process(ProcessInput input) const { conditionalProcess(input); }
+void TrajOptProcessGenerator::process(const ProcessInput& input,
+                                      const Instruction& start_instruction,
+                                      const Instruction& end_instruction) const
+{
+  conditionalProcess(input, start_instruction, end_instruction);
+}
 
 }  // namespace tesseract_planning
