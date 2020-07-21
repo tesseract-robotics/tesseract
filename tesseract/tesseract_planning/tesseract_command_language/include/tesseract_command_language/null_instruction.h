049345890f053dfd988b5463b8bc4d5a4490cf75
Levi Armstrong <levi.armstrong@gmail.com>
2020-07-21 09:18:53 -0500

    Add missing license and warnings macro to files

--- tesseract/tesseract_planning/tesseract_command_language/include/tesseract_command_language/null_instruction.h
+++ tesseract/tesseract_planning/tesseract_command_language/include/tesseract_command_language/null_instruction.h
@@ -1,8 +1,37 @@
+/**
+ * @file null_instruction.h
+ * @brief
+ *
+ * @author Levi Armstrong
+ * @date June 15, 2020
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2020, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef TESSERACT_COMMAND_LANGUAGE_NULL_INSTRUCTION_H
 #define TESSERACT_COMMAND_LANGUAGE_NULL_INSTRUCTION_H
 
-#include <tesseract_command_language/instruction_type.h>
+#include <tesseract_common/macros.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
 #include <iostream>
+TESSERACT_COMMON_IGNORE_WARNINGS_POP
+
+#include <tesseract_command_language/instruction_type.h>
 
 namespace tesseract_planning
 {
