a0d42ef8ec8ecc72ff38fc791f0092623d61666b
Levi Armstrong <levi.armstrong@gmail.com>
2020-08-06 17:35:17 -0500

    Rename sequential_failure_tree_taskflow to sequential_taskflow

--- tesseract/tesseract_planning/tesseract_process_managers/src/process_managers/raster_process_manager.cpp
+++ tesseract/tesseract_planning/tesseract_process_managers/src/process_managers/raster_process_manager.cpp
@@ -11,7 +11,7 @@ TESSERACT_COMMON_IGNORE_WARNINGS_POP
 
 #include <tesseract_process_managers/process_generators/random_process_generator.h>
 #include <tesseract_process_managers/process_generators/motion_planner_process_generator.h>
-#include <tesseract_process_managers/taskflow_generators/sequential_failure_tree_taskflow.h>
+#include <tesseract_process_managers/taskflow_generators/sequential_taskflow.h>
 #include <tesseract_process_managers/process_managers/default_processes/default_freespace_processes.h>
 #include <tesseract_process_managers/process_managers/default_processes/default_raster_processes.h>
 #include <tesseract_process_managers/process_managers/raster_process_manager.h>
