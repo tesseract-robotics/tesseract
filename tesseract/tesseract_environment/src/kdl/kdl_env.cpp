4cd2d6ce525053494d0bf6fd0242e1c19f47c23a
Matthew Powelson <powelson.matthew@gmail.com>
2020-08-14 17:03:58 -0600

    Add clone method to Environment

--- tesseract/tesseract_environment/src/kdl/kdl_env.cpp
+++ tesseract/tesseract_environment/src/kdl/kdl_env.cpp
@@ -33,4 +33,33 @@ bool KDLEnv::init(tesseract_scene_graph::SceneGraph::Ptr scene_graph)
   return Environment::create<KDLStateSolver>(scene_graph);
 }
 
+Environment::Ptr KDLEnv::clone() const
+{
+  auto cloned_env = std::make_shared<KDLEnv>();
+
+  std::lock_guard<std::mutex> lock(mutex_);
+  cloned_env->initialized_ = initialized_;
+  cloned_env->revision_ = revision_;
+  cloned_env->commands_ = commands_;
+  cloned_env->scene_graph_ = scene_graph_->clone();
+  cloned_env->scene_graph_const_ = cloned_env->scene_graph_;
+  cloned_env->current_state_ = std::make_shared<EnvState>(*current_state_);
+  cloned_env->state_solver_ = state_solver_->clone();
+  cloned_env->link_names_ = link_names_;
+  cloned_env->joint_names_ = joint_names_;
+  cloned_env->active_link_names_ = active_link_names_;
+  cloned_env->active_joint_names_ = active_joint_names_;
+  cloned_env->is_contact_allowed_fn_ = is_contact_allowed_fn_;
+  if (discrete_manager_)
+    cloned_env->discrete_manager_ = discrete_manager_->clone();
+  if (continuous_manager_)
+    cloned_env->continuous_manager_ = continuous_manager_->clone();
+  cloned_env->discrete_manager_name_ = discrete_manager_name_;
+  cloned_env->continuous_manager_name_ = continuous_manager_name_;
+  cloned_env->discrete_factory_ = discrete_factory_;
+  cloned_env->continuous_factory_ = continuous_factory_;
+
+  return cloned_env;
+}
+
 }  // namespace tesseract_environment
