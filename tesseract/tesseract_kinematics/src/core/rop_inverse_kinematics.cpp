0ef6c7668b417a4611bf4796a41bf6e8105cf774
Levi Armstrong <levi.armstrong@gmail.com>
2020-09-22 10:45:13 -0500

    Fix rep and rop active links and filtering duplicates

--- tesseract/tesseract_kinematics/src/core/rop_inverse_kinematics.cpp
+++ tesseract/tesseract_kinematics/src/core/rop_inverse_kinematics.cpp
@@ -282,8 +282,16 @@ bool RobotOnPositionerInvKin::init(tesseract_scene_graph::SceneGraph::ConstPtr s
   const auto& manip_links = manip_inv_kin_->getLinkNames();
   link_names_.insert(link_names_.end(), manip_links.begin(), manip_links.end());
 
+  // Remove duplicates
+  std::sort(link_names_.begin(), link_names_.end());
+  link_names_.erase(std::unique(link_names_.begin(), link_names_.end()), link_names_.end());
+
   active_link_names_ = positioner_fwd_kin_->getActiveLinkNames();
   active_link_names_.insert(active_link_names_.end(), manip_links.begin(), manip_links.end());
+
+  // Remove duplicates
+  std::sort(active_link_names_.begin(), active_link_names_.end());
+  active_link_names_.erase(std::unique(active_link_names_.begin(), active_link_names_.end()), active_link_names_.end());
 
   auto positioner_num_joints = static_cast<int>(positioner_fwd_kin_->numJoints());
   const Eigen::MatrixX2d& positioner_limits = positioner_fwd_kin_->getLimits().joint_limits;
