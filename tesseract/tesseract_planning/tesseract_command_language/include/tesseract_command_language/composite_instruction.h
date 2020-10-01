0c5d771890264d731d6864f51f5c213286eb5db2
Levi Armstrong <levi.armstrong@gmail.com>
2020-10-01 08:50:22 -0500

    Remove inheritance of std::vector from Composite Instruction

--- tesseract/tesseract_planning/tesseract_command_language/include/tesseract_command_language/composite_instruction.h
+++ tesseract/tesseract_planning/tesseract_command_language/include/tesseract_command_language/composite_instruction.h
@@ -47,7 +47,7 @@ enum class CompositeInstructionOrder
   ORDERED_AND_REVERABLE  // Can go forward or reverse the order
 };
 
-class CompositeInstruction : public std::vector<Instruction>
+class CompositeInstruction
 {
 public:
   using Ptr = std::shared_ptr<CompositeInstruction>;
@@ -81,7 +81,138 @@ public:
 
   tinyxml2::XMLElement* toXML(tinyxml2::XMLDocument& doc) const;
 
+  // C++ container support
+
+  /** value_type */
+  using value_type = Instruction;
+  /** pointer */
+  using pointer = typename std::vector<value_type>::pointer;
+  /** const_pointer */
+  using const_pointer = typename std::vector<value_type>::const_pointer;
+  /** reference */
+  using reference = typename std::vector<value_type>::reference;
+  /** const_reference */
+  using const_reference = typename std::vector<value_type>::const_reference;
+  /** size_type */
+  using size_type = typename std::vector<value_type>::size_type;
+  /** difference_type */
+  using difference_type = typename std::vector<value_type>::difference_type;
+  /** iterator */
+  using iterator = typename std::vector<value_type>::iterator;
+  /** const_iterator */
+  using const_iterator = typename std::vector<value_type>::const_iterator;
+  /** reverse_iterator */
+  using reverse_iterator = typename std::vector<value_type>::reverse_iterator;
+  /** const_reverse_iterator */
+  using const_reverse_iterator = typename std::vector<value_type>::const_reverse_iterator;
+
+  ///////////////
+  // Iterators //
+  ///////////////
+  /** @brief returns an iterator to the beginning */
+  iterator begin();
+  /** @brief returns an iterator to the beginning */
+  const_iterator begin() const;
+  /** @brief returns an iterator to the end */
+  iterator end();
+  /** @brief returns an iterator to the end */
+  const_iterator end() const;
+  /** @brief returns a reverse iterator to the beginning */
+  reverse_iterator rbegin();
+  /** @brief returns a reverse iterator to the beginning */
+  const_reverse_iterator rbegin() const;
+  /** @brief returns a reverse iterator to the end */
+  reverse_iterator rend();
+  /** @brief returns a reverse iterator to the end */
+  const_reverse_iterator crend() const;
+  /** @brief returns a reverse iterator to the end */
+  const_reverse_iterator rend() const;
+  /** @brief returns an iterator to the beginning */
+  const_iterator cbegin() const;
+  /** @brief returns an iterator to the end */
+  const_iterator cend() const;
+  /** @brief returns a reverse iterator to the beginning */
+  const_reverse_iterator const crbegin();
+  /** @brief returns a reverse iterator to the end */
+  const_reverse_iterator const crend();
+
+  //////////////
+  // Capacity //
+  //////////////
+  /** @brief checks whether the container is empty */
+  bool empty() const;
+  /** @brief returns the number of elements */
+  size_type size() const;
+  /** @brief returns the maximum possible number of elements */
+  size_type max_size() const;
+  /** @brief reserve number of elements */
+  void reserve(size_type n);
+  /** @brief returns the number of elements that can be held in currently allocated storage */
+  size_type capacity() const;
+  /** @brief reduces memory usage by freeing unused memory  */
+  void shrink_to_fit();
+
+  ////////////////////
+  // Element Access //
+  ////////////////////
+  /** @brief access the first element */
+  reference front();
+  /** @brief access the first element */
+  const_reference front() const;
+  /** @brief access the last element */
+  reference back();
+  /** @brief access the last element */
+  const_reference back() const;
+  /** @brief access specified element with bounds checking */
+  reference at(size_type n);
+  /** @brief access specified element with bounds checking */
+  const_reference at(size_type n) const;
+  /** @brief direct access to the underlying array  */
+  pointer data();
+  /** @brief direct access to the underlying array  */
+  const_pointer data() const;
+  /** @brief access specified element */
+  reference operator[](size_type pos);
+  /** @brief access specified element */
+  const_reference operator[](size_type pos) const;
+
+  ///////////////
+  // Modifiers //
+  ///////////////
+  /** @brief clears the contents */
+  void clear();
+  /** @brief inserts element */
+  iterator insert(const_iterator p, const value_type& x);
+  iterator insert(const_iterator p, value_type&& x);
+  iterator insert(const_iterator p, std::initializer_list<value_type> l);
+
+  /** @brief constructs element in-place */
+  template <class... Args>
+  iterator emplace(const_iterator pos, Args&&... args);
+
+  /** @brief erases element */
+  iterator erase(const_iterator p);
+  iterator erase(const_iterator first, const_iterator last);
+  /** @brief adds an element to the end */
+  void push_back(const value_type& x);
+  void push_back(const value_type&& x);
+
+  /** @brief constructs an element in-place at the end  */
+  template <typename... Args>
+#if __cplusplus > 201402L
+  reference emplace_back(Args&&... args);
+#else
+  void emplace_back(Args&&... args);
+#endif
+
+  /** @brief removes the last element */
+  void pop_back();
+  /** @brief swaps the contents  */
+  void swap(std::vector<value_type>& other);
+
 private:
+  std::vector<value_type> container_;
+
   int type_{ static_cast<int>(InstructionType::COMPOSITE_INSTRUCTION) };
 
   /** @brief The description of the instruction */
@@ -106,7 +237,7 @@ private:
    *
    * If not provided, the planner should use the current state of the robot is used and defined as fixed.
    */
-  Instruction start_instruction_{ NullInstruction() };
+  value_type start_instruction_{ NullInstruction() };
 };
 
 }  // namespace tesseract_planning
