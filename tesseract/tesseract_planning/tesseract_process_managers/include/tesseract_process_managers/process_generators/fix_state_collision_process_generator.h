4fca8babb7d202e2634ceb3ecd4ccf0d579059e3
Matthew Powelson <mpowelson@users.noreply.github.com>
2020-11-05 11:08:25 -0700

    Add MoveWaypointFromCollisionRandomSampler to FixStateCollisionProcess (#426)
    * Add MoveWaypointFromCollisionRandomSampler to FixStateCollisionProcess
    
    * Add more generalized way of specifying correction methods
    
    * Bug fix
    
    * Add assert to catch mismatched sizes
    
    * Rebase fixes and a bug fix

--- tesseract/tesseract_planning/tesseract_process_managers/include/tesseract_process_managers/process_generators/fix_state_collision_process_generator.h
+++ tesseract/tesseract_planning/tesseract_process_managers/include/tesseract_process_managers/process_generators/fix_state_collision_process_generator.h
@@ -48,22 +48,39 @@ struct FixStateCollisionProfile
     DISABLED
   };
 
+  /** @brief Used to specify method used to correct states in collision */
+  enum class CorrectionMethod
+  {
+    NONE,
+    TRAJOPT,
+    RANDOM_SAMPLER
+  };
+
   FixStateCollisionProfile(Settings mode = Settings::ALL) : mode(mode) {}
 
   /** @brief Sets which terms will be corrected  */
   Settings mode;
+
+  /** @brief Order that correction methods will be applied. These will be attempted in order until one succeeds or all
+   * have been tried */
+  std::vector<CorrectionMethod> correction_workflow{ CorrectionMethod::TRAJOPT, CorrectionMethod::RANDOM_SAMPLER };
 
   /** @brief Percent of the total joint range that a joint will be allowed to be adjusted */
   double jiggle_factor{ 0.02 };
 
   /** @brief Safety margin applied to collision costs/cnts when using trajopt to correct collisions */
   double safety_margin{ 0.025 };
+
+  /** @brief Number of sampling attempts if TrajOpt correction fails*/
+  int sampling_attempts{ 100 };
 };
 using FixStateCollisionProfileMap = std::unordered_map<std::string, FixStateCollisionProfile::Ptr>;
 
 /**
  * @brief This generator modifies the const input instructions in order to push waypoints that are in collision out of
- * collision
+ * collision.
+ *
+ * First it uses TrajOpt to correct the waypoint. If that fails, it reverts to random sampling
  */
 class FixStateCollisionProcessGenerator : public ProcessGenerator
 {
@@ -100,5 +117,47 @@ private:
 
   void process(ProcessInput input) const;
 };
+
+/**
+ * @brief Checks if a joint state is in collision
+ * @param start_pos Vector that represents a joint state
+ * @param input Process Input associated with waypoint. Needed for kinematics, etc.
+ * @return True if in collision
+ */
+bool StateInCollision(const Eigen::Ref<const Eigen::VectorXd>& start_pos,
+                      const ProcessInput& input,
+                      const FixStateCollisionProfile& profile);
+
+/**
+ * @brief Checks if a waypoint is in collision
+ * @param waypoint Must be a waypoint for which getJointPosition will return a position
+ * @param input Process Input associated with waypoint. Needed for kinematics, etc.
+ * @return True if in collision
+ */
+bool WaypointInCollision(const Waypoint& waypoint, const ProcessInput& input, const FixStateCollisionProfile& profile);
+
+/**
+ * @brief Takes a waypoint and uses a small trajopt problem to push it out of collision if necessary
+ * @param waypoint Must be a waypoint for which getJointPosition will return a position
+ * @param input Process Input associated with waypoint. Needed for kinematics, etc.
+ * @param profile Profile containing needed params
+ * @return True if successful
+ */
+bool MoveWaypointFromCollisionTrajopt(Waypoint& waypoint,
+                                      const ProcessInput& input,
+                                      const FixStateCollisionProfile& profile);
+
+/**
+ * @brief Takes a waypoint and uses random sampling to find a position that is out of collision
+ * @param waypoint Must be a waypoint for which getJointPosition will return a position
+ * @param input Process Input associated with waypoint. Needed for kinematics, etc.
+ * @param profile Profile containing needed params
+ * @return True if successful
+ */
+bool MoveWaypointFromCollisionRandomSampler(Waypoint& waypoint,
+                                            const ProcessInput& input,
+                                            const FixStateCollisionProfile& profile);
+
+bool ApplyCorrectionWorkflow(Waypoint& waypoint, const ProcessInput& input, const FixStateCollisionProfile& profile);
 }  // namespace tesseract_planning
 #endif  // TESSERACT_PROCESS_MANAGERS_FIX_STATE_BOUNDS_PROCESS_GENERATOR_H
