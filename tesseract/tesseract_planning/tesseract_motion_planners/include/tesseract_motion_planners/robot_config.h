aea842ddbe8bb6cfcb81d5ae130e5380a818670e
David Merz, Jr <david.merz@swri.org>
2020-12-21 15:06:31 -0600

    Clang Fix

--- tesseract/tesseract_planning/tesseract_motion_planners/include/tesseract_motion_planners/robot_config.h
+++ tesseract/tesseract_planning/tesseract_motion_planners/include/tesseract_motion_planners/robot_config.h
@@ -128,11 +128,12 @@ inline RobotConfig getRobotConfig(const tesseract_kinematics::ForwardKinematics:
 
   return RobotConfig::NDB;
 }
-template <typename FloatType>
-inline Eigen::Vector3i getRobotRedundancy(const Eigen::Ref<const Eigen::Matrix<FloatType, Eigen::Dynamic, 1>>& joint_values,
-                                     const Eigen::Ref<const Eigen::Vector2i>& sign_correction = Eigen::Vector2i::Ones())
-{
 
+template <typename FloatType>
+inline Eigen::Vector3i
+getRobotRedundancy(const Eigen::Ref<const Eigen::Matrix<FloatType, Eigen::Dynamic, 1>>& joint_values,
+                   const Eigen::Ref<const Eigen::Vector2i>& sign_correction = Eigen::Vector2i::Ones())
+{
   Eigen::Vector3i redundancy;
 
   int red_j_4 = int(joint_values(3) / M_PI);
