026bb2ff7f9c12e272a5e3d9e2e24711980f105e
John Wason <wason@wasontech.com>
2021-01-23 21:21:12 -0500

    Add SWIG shared_ptr commands to visualization markers

--- tesseract_visualization/include/tesseract_visualization/markers/axis_marker.h
+++ tesseract_visualization/include/tesseract_visualization/markers/axis_marker.h
@@ -3,6 +3,10 @@
 
 #include <tesseract_visualization/markers/marker.h>
 
+#ifdef SWIG
+%shared_ptr(tesseract_visualization::AxisMarker)
+#endif  // SWIG
+
 namespace tesseract_visualization
 {
 /** @brief An axis */
