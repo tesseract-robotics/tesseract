ea5241de37b5ac39ec739d496acbcd554944ecb7
Levi Armstrong <levi.armstrong@swri.org>
2021-02-14 14:13:40 -0600

    Improve clone cache unit tests and fix issues with getting clone

--- tesseract_common/test/clone_cache_unit.cpp
+++ tesseract_common/test/clone_cache_unit.cpp
@@ -63,12 +63,34 @@ public:
   }
 };
 
+class TestObjectSupportsUpdateFailure : public TestObject
+{
+public:
+  using Ptr = std::shared_ptr<TestObjectSupportsUpdateFailure>;
+  using ConstPtr = std::shared_ptr<const TestObjectSupportsUpdateFailure>;
+
+  bool update(const TestObjectSupportsUpdateFailure::ConstPtr& pattern)
+  {
+    val_1 = pattern->val_1;
+    val_2 = pattern->val_2;
+    revision_ = pattern->revision_;
+    return true;
+  }
+
+  TestObjectSupportsUpdateFailure::Ptr clone() const
+  {
+    throw std::runtime_error("TestObjectSupportsUpdateFailure: clone failed!");
+  }
+};
+
 TEST(TesseractCloneCacheUnit, WithoutUpdate)  // NOLINT
 {
   auto original = std::make_shared<TestObject>();
   original->val_1 = 1;
   original->val_2 = 2;
   auto clone_cache = std::make_shared<CloneCache<TestObject>>(original, 3);
+  EXPECT_EQ(clone_cache->getCacheSize(), 3);
+  EXPECT_EQ(clone_cache->getCurrentCacheSize(), 3);
 
   // Baseline clone of original
   {
@@ -90,6 +112,27 @@ TEST(TesseractCloneCacheUnit, WithoutUpdate)  // NOLINT
     original->revision_++;
     EXPECT_NE(original->val_1, clone->val_1);
   }
+  // Cache should be empty so call update
+  {
+    EXPECT_EQ(clone_cache->getCurrentCacheSize(), 0);
+    clone_cache->updateCache();
+    EXPECT_EQ(clone_cache->getCurrentCacheSize(), 3);
+  }
+  // Now change revision and call update
+  {
+    original->revision_++;
+    original->val_1 = 5;
+    clone_cache->updateCache();
+    auto clone = clone_cache->clone();
+    EXPECT_EQ(original->val_1, clone->val_1);
+  }
+  // Now change revision and call clone
+  {
+    original->revision_++;
+    original->val_1 = 6;
+    auto clone = clone_cache->clone();
+    EXPECT_EQ(original->val_1, clone->val_1);
+  }
   // Try cloning more times than the cache is big
   for (int i = 0; i < 10; i++)
   {
@@ -99,6 +142,7 @@ TEST(TesseractCloneCacheUnit, WithoutUpdate)  // NOLINT
   EXPECT_EQ(clone_cache->getCacheSize(), 3);
   clone_cache->setCacheSize(8);
   EXPECT_EQ(clone_cache->getCacheSize(), 8);
+  EXPECT_EQ(clone_cache->getCurrentCacheSize(), 8);
 }
 
 TEST(TesseractCloneCacheUnit, SupportsUpdate)  // NOLINT
@@ -107,6 +151,8 @@ TEST(TesseractCloneCacheUnit, SupportsUpdate)  // NOLINT
   original->val_1 = 1;
   original->val_2 = 2;
   auto clone_cache = std::make_shared<CloneCache<TestObjectSupportsUpdate>>(original, 3);
+  EXPECT_EQ(clone_cache->getCacheSize(), 3);
+  EXPECT_EQ(clone_cache->getCurrentCacheSize(), 3);
 
   // Baseline clone of original
   {
@@ -127,6 +173,30 @@ TEST(TesseractCloneCacheUnit, SupportsUpdate)  // NOLINT
 
     original->revision_++;
     EXPECT_NE(original->val_1, clone->val_1);
+
+    auto updated_clone = clone_cache->clone();
+    EXPECT_EQ(original->val_1, updated_clone->val_1);
+  }
+  // Cache should be empty so call update
+  {
+    EXPECT_EQ(clone_cache->getCurrentCacheSize(), 0);
+    clone_cache->updateCache();
+    EXPECT_EQ(clone_cache->getCurrentCacheSize(), 3);
+  }
+  // Now change revision and call update
+  {
+    original->revision_++;
+    original->val_1 = 5;
+    clone_cache->updateCache();
+    auto clone = clone_cache->clone();
+    EXPECT_EQ(original->val_1, clone->val_1);
+  }
+  // Now change revision and call clone
+  {
+    original->revision_++;
+    original->val_1 = 6;
+    auto clone = clone_cache->clone();
+    EXPECT_EQ(original->val_1, clone->val_1);
   }
   // Try cloning more times than the cache is big
   for (int i = 0; i < 10; i++)
@@ -137,6 +207,42 @@ TEST(TesseractCloneCacheUnit, SupportsUpdate)  // NOLINT
   EXPECT_EQ(clone_cache->getCacheSize(), 3);
   clone_cache->setCacheSize(8);
   EXPECT_EQ(clone_cache->getCacheSize(), 8);
+  EXPECT_EQ(clone_cache->getCurrentCacheSize(), 8);
+}
+
+TEST(TesseractCloneCacheUnit, SupportsUpdateFailure)  // NOLINT
+{
+  {  // Test original is a nullptr
+    std::shared_ptr<TestObjectSupportsUpdate> original;
+    auto clone_cache = std::make_shared<CloneCache<TestObjectSupportsUpdate>>(original, 3);
+    EXPECT_TRUE(clone_cache->clone() == nullptr);
+    EXPECT_EQ(clone_cache->getCacheSize(), 3);
+    EXPECT_EQ(clone_cache->getCurrentCacheSize(), 0);
+    clone_cache->updateCache();
+    EXPECT_EQ(clone_cache->getCurrentCacheSize(), 0);
+  }
+
+  // Test when clone throws an exception
+  auto original = std::make_shared<TestObjectSupportsUpdateFailure>();
+  original->val_1 = 1;
+  original->val_2 = 2;
+  auto clone_cache = std::make_shared<CloneCache<TestObjectSupportsUpdateFailure>>(original, 3);
+  EXPECT_EQ(clone_cache->getCacheSize(), 3);
+  EXPECT_EQ(clone_cache->getCurrentCacheSize(), 0);
+
+  // Baseline clone of original which should be nullptr because of exception during clone
+  {
+    EXPECT_TRUE(clone_cache->clone() == nullptr);
+    EXPECT_EQ(clone_cache->getCacheSize(), 3);
+    EXPECT_EQ(clone_cache->getCurrentCacheSize(), 0);
+  }
+  // Now it should update the cache and clone should be nullptr because of exception during clone
+  {
+    original->revision_++;
+    EXPECT_EQ(clone_cache->getCacheSize(), 3);
+    EXPECT_EQ(clone_cache->getCurrentCacheSize(), 0);
+    EXPECT_TRUE(clone_cache->clone() == nullptr);
+  }
 }
 
 int main(int argc, char** argv)
