42021f06a0e7c578f65aac0b7021526292c38b6a
Levi-Armstrong <levi.armstrong@gmail.com>
2021-09-29 21:56:58 -0500

    Rebase on upstream/master

--- tesseract_geometry/include/tesseract_geometry/impl/polygon_mesh.h
+++ tesseract_geometry/include/tesseract_geometry/impl/polygon_mesh.h
@@ -32,9 +32,9 @@ TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
 #include <memory>
 TESSERACT_COMMON_IGNORE_WARNINGS_POP
 
-#include <tesseract_geometry/geometry.h>
 #include <tesseract_common/types.h>
-#include <tesseract_common/resource.h>
+#include <tesseract_common/resource_locator.h>
+#include <tesseract_geometry/geometry.h>
 #include <tesseract_geometry/impl/mesh_material.h>
 
 #ifdef SWIG
