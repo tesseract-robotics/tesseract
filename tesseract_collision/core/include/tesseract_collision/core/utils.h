84c47852f06e27af2892d432f991cf4cd289d4e5
Matthew Powelson <powelson.matthew@gmail.com>
2021-12-01 16:40:47 -0700

    Add AllowedCollisionMatrix to CollisionCheckConfig

---
+++ tesseract_collision/core/include/tesseract_collision/core/utils.h
@@ -0,0 +1,69 @@
+/**
+ * @file utils.h
+ * @brief Tesseract Collision utils
+ *
+ * @author Levi Armstrong
+ * @author Matthew Powelson
+ * @date December 1, 2021
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef TESSERACT_COLLISION_CORE_UTILS_H
+#define TESSERACT_COLLISION_CORE_UTILS_H
+
+#include <tesseract_common/macros.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
+#include <tesseract_collision/core/types.h>
+#include <tesseract_collision/core/continuous_contact_manager.h>
+#include <tesseract_collision/core/discrete_contact_manager.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_POP
+
+namespace tesseract_collision
+{
+/**
+ * @brief Combines two IsContactAllowedFns using the override type
+ * @param original Original IsContactAllowedFns. This will be returned if ACMOverrideType is None
+ * @param override Overriding IsContactAllowedFns. This will be returned if ACMOverrideType is ASSIGN
+ * @param type Override type used to combine the IsContactAllowedFns
+ * @return One IsContactAllowedFn that combines the two
+ */
+IsContactAllowedFn combineContactAllowedFn(const IsContactAllowedFn& original,
+                                           const IsContactAllowedFn& override,
+                                           ACMOverrideType type = ACMOverrideType::OR);
+
+/**
+ * @brief Applies ACM to contact manager using override type
+ * @param manager Manager whose IsContactAllowedFn will be overwritten
+ * @param acm ACM used to create IsContactAllowedFn
+ * @param type Determines how the two IsContactAllowedFns are combined
+ */
+void applyIsContactAllowedFnOverride(tesseract_collision::ContinuousContactManager& manager,
+                                     const tesseract_common::AllowedCollisionMatrix& acm,
+                                     ACMOverrideType type = ACMOverrideType::OR);
+/**
+ * @brief Applies ACM to contact manager using override type
+ * @param manager Manager whose IsContactAllowedFn will be overwritten
+ * @param acm ACM used to create IsContactAllowedFn
+ * @param type Determines how the two IsContactAllowedFns are combined
+ */
+void applyIsContactAllowedFnOverride(tesseract_collision::DiscreteContactManager& manager,
+                                     const tesseract_common::AllowedCollisionMatrix& acm,
+                                     ACMOverrideType type = ACMOverrideType::OR);
+
+}  // namespace tesseract_collision
+
+#endif  // TESSERACT_COLLISION_TYPES_H
