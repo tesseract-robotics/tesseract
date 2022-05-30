41606c6b04b84984348754579dcca3bb9dbae9e0
Levi Armstrong <levi.armstrong@gmail.com>
2022-05-29 20:09:44 -0500

    Update Findtcmalloc.cmake to include threads and split out tcmalloc_minimal to Findtcmalloc_minimal.cmake

---
+++ tesseract_common/cmake/Findtcmalloc_minimal.cmake
@@ -0,0 +1,44 @@
+# * Find tcmalloc_minimal
+#
+# tcmalloc_minimal_LIBRARIES   - List of libraries when using tcmalloc. tcmalloc_minimal_FOUND       - True if
+# tcmalloc_minimal found.
+
+set(tcmalloc_minimal_NAMES
+    tcmalloc_minimal
+    libtcmalloc_minimal
+    libtcmalloc_minimal.so
+    tcmalloc_minimal4
+    libtcmalloc_minimal.so.4)
+
+find_library(
+  tcmalloc_minimal_LIBRARY NO_DEFAULT_PATH
+  NAMES ${tcmalloc_minimal_NAMES}
+  PATHS /lib
+        /usr/lib
+        /usr/lib/x86_64-linux-gnu
+        /usr/local/lib
+        /opt/local/lib)
+
+if(tcmalloc_minimal_LIBRARY)
+  set(tcmalloc_minimal_FOUND TRUE)
+else()
+  set(tcmalloc_minimal_FOUND FALSE)
+endif()
+
+if(tcmalloc_minimal_FOUND)
+  message(STATUS "Found tcmalloc_minimal: ${tcmalloc_minimal_LIBRARY}")
+else()
+  message(STATUS "Not Found tcmalloc_minimal: ${tcmalloc_minimal_LIBRARY} with names ${tcmalloc_minimal_NAMES}")
+endif()
+
+mark_as_advanced(tcmalloc_minimal_LIBRARY)
+
+# Output variables generation
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(tcmalloc_minimal DEFAULT_MSG tcmalloc_minimal_LIBRARY)
+
+if(tcmalloc_FOUND)
+  find_package(Threads REQUIRED)
+  add_library(tcmalloc::tcmalloc_minimal INTERFACE IMPORTED)
+  target_link_libraries(tcmalloc::tcmalloc_minimal INTERFACE ${tcmalloc_LIBRARY} Threads::Threads)
+endif()
