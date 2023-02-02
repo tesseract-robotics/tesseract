8bbabcbb782e38a7458d8e034cfe1db5f61e77b5
Sean Cardello <sean@fourgrowers.com>
2023-02-02 18:14:04 -0500

    - Move config struct into class - Add Eigen alignment - Trivialize assignment operators to avoid storing kdl_config (though I can do this if preferred) - Format

--- tesseract_kinematics/kdl/src/kdl_factories.cpp
+++ tesseract_kinematics/kdl/src/kdl_factories.cpp
@@ -69,7 +69,7 @@ InverseKinematics::UPtr KDLInvKinChainLMAFactory::create(const std::string& solv
 {
   std::string base_link;
   std::string tip_link;
-  KDLConfig kdl_config;
+  KDLInvKinChainLMA::Config kdl_config;
 
   try
   {
@@ -87,12 +87,16 @@ InverseKinematics::UPtr KDLInvKinChainLMAFactory::create(const std::string& solv
     if (YAML::Node n = config["task_weights"])
     {
       // Make sure the length matches the constructor interface
-      if (n.size() != 6)
-        throw std::runtime_error("KDLInvKinChainLMAFactory, size of task_weights needs to be 6");
+      if (n.size() != kdl_config.task_weights.size())
+      {
+        std::ostringstream what;
+        what << "KDLInvKinChainLMAFactory, size of task_weights needs to be " << kdl_config.task_weights.size();
+        throw std::runtime_error(what.str());
+      }
 
       auto w = 0;
-      for (auto const &v : n)
-        kdl_config.weights(w++) = v.as<double>();
+      for (auto const& v : n)
+        kdl_config.task_weights(w++) = v.as<double>();
     }
 
     if (YAML::Node n = config["eps"])
@@ -121,6 +125,7 @@ InverseKinematics::UPtr KDLInvKinChainNRFactory::create(const std::string& solve
 {
   std::string base_link;
   std::string tip_link;
+  KDLInvKinChainNR::Config kdl_config;
 
   try
   {
@@ -133,6 +138,19 @@ InverseKinematics::UPtr KDLInvKinChainNRFactory::create(const std::string& solve
       tip_link = n.as<std::string>();
     else
       throw std::runtime_error("KDLInvKinChainNRFactory, missing 'tip_link' entry");
+
+    // Optional configuration parameters
+    if (YAML::Node n = config["velocity_eps"])
+      kdl_config.vel_eps = n.as<double>();
+
+    if (YAML::Node n = config["velocity_iterations"])
+      kdl_config.vel_iterations = n.as<int>();
+
+    if (YAML::Node n = config["position_eps"])
+      kdl_config.pos_eps = n.as<double>();
+
+    if (YAML::Node n = config["position_iterations"])
+      kdl_config.pos_iterations = n.as<int>();
   }
   catch (const std::exception& e)
   {
@@ -140,7 +158,7 @@ InverseKinematics::UPtr KDLInvKinChainNRFactory::create(const std::string& solve
     return nullptr;
   }
 
-  return std::make_unique<KDLInvKinChainNR>(scene_graph, base_link, tip_link, solver_name);
+  return std::make_unique<KDLInvKinChainNR>(scene_graph, base_link, tip_link, kdl_config, solver_name);
 }
 
 TESSERACT_PLUGIN_ANCHOR_IMPL(KDLFactoriesAnchor)
