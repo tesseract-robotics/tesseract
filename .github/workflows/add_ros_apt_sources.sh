a6354ec755cd080d3cf90948d32e2c4f50c969ce
Michael Ripperger <michael.ripperger@swri.org>
2023-07-11 13:08:07 -0500

    Add Jammy CI Build (#912)
    * Add Jammy build
    
    * Update install script for ROS package management and build tools

--- .github/workflows/add_ros_apt_sources.sh
+++ .github/workflows/add_ros_apt_sources.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
 apt update -qq
