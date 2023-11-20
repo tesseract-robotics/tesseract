34904ef3832a27d3b3f93a4d49b9291fd1f94a0a
Levi Armstrong <levi.armstrong@gmail.com>
2023-11-20 14:01:58 -0600

    Cleanup CI and docker file

--- .github/workflows/add_ros_apt_sources.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
-apt update -qq
