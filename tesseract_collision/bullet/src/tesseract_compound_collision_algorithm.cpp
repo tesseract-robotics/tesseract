f59b78c9939aab83eec26d500012cbe3e2eaf450
Roelof Oomen <rjoomen@yahoo.com>
2024-05-31 13:18:51 +0200

    Fix 2 for older bullet versions (e.g. on Ubuntu Focal)

--- tesseract_collision/bullet/src/tesseract_compound_collision_algorithm.cpp
+++ tesseract_collision/bullet/src/tesseract_compound_collision_algorithm.cpp
@@ -164,13 +164,12 @@ public:
 
     if (TestAabbAgainstAabb2(aabbMin0, aabbMax0, aabbMin1, aabbMax1))
     {
-      btTransform preTransform = childTrans;
 #if BT_BULLET_VERSION >= 300
+      btTransform preTransform = childTrans;
       if (this->m_compoundColObjWrap->m_preTransform != nullptr)
       {
         preTransform = preTransform * (*(this->m_compoundColObjWrap->m_preTransform));
       }
-#endif
       btCollisionObjectWrapper compoundWrap(this->m_compoundColObjWrap,
                                             childShape,
                                             m_compoundColObjWrap->getCollisionObject(),
@@ -178,6 +177,14 @@ public:
                                             preTransform,
                                             -1,
                                             index);
+#else
+      btCollisionObjectWrapper compoundWrap(this->m_compoundColObjWrap,
+                                            childShape,
+                                            m_compoundColObjWrap->getCollisionObject(),
+                                            newChildWorldTrans,
+                                            -1,
+                                            index);
+#endif
 
       btCollisionAlgorithm* algo = nullptr;
       bool allocatedAlgorithm = false;
