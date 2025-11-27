fc69077c3aed15b85db53535105f3b66149016a5
Levi Armstrong <levi.armstrong@gmail.com>
2025-11-26 21:51:15 -0600

    Add doxygen to tesseract collision example

--- tesseract_collision/examples/box_box_example.cpp
+++ tesseract_collision/examples/box_box_example.cpp
@@ -1,3 +1,100 @@
+/**
+ * @page box_box_example Tesseract Collision Checker Example
+ *
+ * This example shows how to use Tesseract collision checker
+ * to perform discrete collision and distance queries between simple shapes.
+ *
+ * It demonstrates how to:
+ *
+ * - Create a Bullet-based discrete BVH collision manager
+ * - Add primitive box and convex-hull collision shapes
+ * - Configure active collision objects and collision margins
+ * - Set object transforms
+ * - Query for closest distances and interpret the results
+ *
+ * @section box_box_example_manager Creating the collision manager
+ *
+ * First, create the Bullet discrete BVH collision manager. This object owns the
+ * underlying Bullet broadphase data structures and provides the main API for
+ * adding collision objects and performing contact tests.
+ *
+ * @snippet box_box_example.cpp collision_example_create_manager
+ *
+ * @section box_box_example_add_primitive Adding primitive boxes
+ *
+ * Add a unit box (1 x 1 x 1) as a collision object:
+ *
+ * @snippet box_box_example.cpp collision_example_add_box
+ *
+ * We also add a second, thinner box (0.1 x 1 x 1), but leave it disabled so it
+ * does not participate in collision queries:
+ *
+ * @snippet box_box_example.cpp collision_example_add_thin_box
+ *
+ * @section box_box_example_convex_hull Adding a convex-hull object
+ *
+ * Next, we load a mesh from a PLY file, convert it to a convex hull, and add it
+ * as another collision object:
+ *
+ * @snippet box_box_example.cpp collision_example_add_convex_hull
+ * @snippet box_box_example.cpp collision_example_add_convex_hull_collision
+ *
+ * @section box_box_example_setup Setting active objects and margin
+ *
+ * Before running a collision check, choose which objects should be active and
+ * configure the collision margin:
+ *
+ * @snippet box_box_example.cpp collision_example_set_active_objects
+ * @snippet box_box_example.cpp collision_example_set_margin_data
+ *
+ * @section box_box_example_initial_poses Setting initial transforms
+ *
+ * Set the initial transforms for the active objects:
+ *
+ * @snippet box_box_example.cpp collision_example_set_transforms_initial
+ *
+ * @section box_box_example_first_check First collision/distance check
+ *
+ * Perform a closest-distance query and inspect the resulting distance, nearest
+ * points, and normal:
+ *
+ * @snippet box_box_example.cpp collision_example_first_check
+ *
+ * @section box_box_example_move_outside Moving out of contact distance
+ *
+ * Move one object further away so it lies outside the current contact distance
+ * threshold and repeat the check:
+ *
+ * @snippet box_box_example.cpp collision_example_move_outside_contact_distance
+ * @snippet box_box_example.cpp collision_example_second_check
+ *
+ * @section box_box_example_increase_margin Increasing the contact distance
+ *
+ * Increase the default collision margin and run another check to see how the
+ * results change:
+ *
+ * @snippet box_box_example.cpp collision_example_change_default_margin
+ * @snippet box_box_example.cpp collision_example_third_check
+ *
+ * @section box_box_example_summary Summary
+ *
+ * This example demonstrates the core workflow when using
+ * `BulletDiscreteBVHManager`:
+ *
+ * - Create a manager and register your collision objects (primitive and/or convex hull)
+ * - Choose which objects are active for a given query
+ * - Set collision margins and object transforms
+ * - Call `contactTest` and interpret the results to make decisions in your
+ *   planner or controller
+ *
+ * @section box_box_example_full_code Full source code
+ *
+ * For reference, here is the complete source of this example:
+ *
+ * @snippet box_box_example.cpp box_box_example_full_source
+ */
+
+//! [box_box_example_full_source]
 #include <tesseract_common/macros.h>
 TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
 #include <console_bridge/console.h>
@@ -23,12 +120,11 @@ std::string toString(bool b) { return b ? "true" : "false"; }
 
 int main(int /*argc*/, char** /*argv*/)
 {
-  // documentation:start:1: Create Collision Manager
+  //! [collision_example_create_manager]
   tesseract_collision_bullet::BulletDiscreteBVHManager checker;
-  // documentation:end:1: Create Collision Manager
+  //! [collision_example_create_manager]
 
-  // documentation:start:2: Add box to checker
-  // Create a box
+  //! [collision_example_add_box]
   CollisionShapePtr box = std::make_shared<Box>(1, 1, 1);
   Eigen::Isometry3d box_pose;
   box_pose.setIdentity();
@@ -38,12 +134,10 @@ int main(int /*argc*/, char** /*argv*/)
   obj1_shapes.push_back(box);
   obj1_poses.push_back(box_pose);
 
-  // Add box to checker in enabled state
   checker.addCollisionObject("box_link", 0, obj1_shapes, obj1_poses);
-  // documentation:end:2: Add box to checker
+  //! [collision_example_add_box]
 
-  // documentation:start:3: Add thin box
-  // Create a thin box
+  //! [collision_example_add_thin_box]
   CollisionShapePtr thin_box = std::make_shared<Box>(0.1, 1, 1);
   Eigen::Isometry3d thin_box_pose;
   thin_box_pose.setIdentity();
@@ -53,12 +147,10 @@ int main(int /*argc*/, char** /*argv*/)
   obj2_shapes.push_back(thin_box);
   obj2_poses.push_back(thin_box_pose);
 
-  // Add thin box to checker in disabled state
   checker.addCollisionObject("thin_box_link", 0, obj2_shapes, obj2_poses, false);
-  // documentation:end:3: Add thin box
+  //! [collision_example_add_thin_box]
 
-  // documentation:start:4: Add convex hull
-  // Add second box to checker, but convert to convex hull mesh
+  //! [collision_example_add_convex_hull]
   tesseract_common::GeneralResourceLocator locator;
   CollisionShapePtr second_box;
 
@@ -72,9 +164,9 @@ int main(int /*argc*/, char** /*argv*/)
 
   auto mesh = std::make_shared<tesseract_geometry::Mesh>(mesh_vertices, mesh_faces);
   second_box = makeConvexMesh(*mesh);
-  // documentation:end:4: Add convex hull
+  //! [collision_example_add_convex_hull]
 
-  // documentation:start:5: Add convex hull collision
+  //! [collision_example_add_convex_hull_collision]
   Eigen::Isometry3d second_box_pose;
   second_box_pose.setIdentity();
 
@@ -84,19 +176,19 @@ int main(int /*argc*/, char** /*argv*/)
   obj3_poses.push_back(second_box_pose);
 
   checker.addCollisionObject("second_box_link", 0, obj3_shapes, obj3_poses);
-  // documentation:end:5: Add convex hull collision
+  //! [collision_example_add_convex_hull_collision]
 
   CONSOLE_BRIDGE_logInform("Test when object is inside another");
-  // documentation:start:6: Set active collision object
+
+  //! [collision_example_set_active_objects]
   checker.setActiveCollisionObjects({ "box_link", "second_box_link" });
-  // documentation:end:6: Set active collision object
+  //! [collision_example_set_active_objects]
 
-  // documentation:start:7: Set contact distance threshold
+  //! [collision_example_set_margin_data]
   checker.setCollisionMarginData(CollisionMarginData(0.1));
-  // documentation:end:7: Set contact distance threshold
+  //! [collision_example_set_margin_data]
 
-  // documentation:start:8: Set collision object transform
-  // Set the collision object transforms
+  //! [collision_example_set_transforms_initial]
   tesseract_common::TransformMap location;
   location["box_link"] = Eigen::Isometry3d::Identity();
   location["box_link"].translation()(0) = 0.2;
@@ -104,9 +196,9 @@ int main(int /*argc*/, char** /*argv*/)
   location["second_box_link"] = Eigen::Isometry3d::Identity();
 
   checker.setCollisionObjectsTransform(location);
-  // documentation:end:8: Set collision object transform
+  //! [collision_example_set_transforms_initial]
 
-  // documentation:start:9: Perform collision check
+  //! [collision_example_first_check]
   ContactResultMap result;
   ContactRequest request(ContactTestType::CLOSEST);
   checker.contactTest(result, request);
@@ -126,35 +218,33 @@ int main(int /*argc*/, char** /*argv*/)
                            result_vector[0].link_names[0].c_str(),
                            result_vector[0].link_names[1].c_str(),
                            toString(result_vector[0].normal).c_str());
-  // documentation:end:9: Perform collision check
+  //! [collision_example_first_check]
 
-  // documentation:start:10: Set collision object transform
   CONSOLE_BRIDGE_logInform("Test object is out side the contact distance");
+
+  //! [collision_example_move_outside_contact_distance]
   location["box_link"].translation() = Eigen::Vector3d(1.60, 0, 0);
   checker.setCollisionObjectsTransform(location);
-  // documentation:end:10: Set collision object transform
+  //! [collision_example_move_outside_contact_distance]
 
-  // documentation:start:11: Perform collision check
+  //! [collision_example_second_check]
   result.clear();
   result_vector.clear();
 
-  // Check for collision after moving object
   checker.contactTest(result, request);
   result.flattenMoveResults(result_vector);
   CONSOLE_BRIDGE_logInform("Has collision: %s", toString(result_vector.empty()).c_str());
-  // documentation:end:11: Perform collision check
+  //! [collision_example_second_check]
 
-  // documentation:start:12: Change contact distance threshold
-  // Set higher contact distance threshold
+  //! [collision_example_change_default_margin]
   checker.setDefaultCollisionMargin(0.25);
-  // documentation:end:12: Change contact distance threshold
+  //! [collision_example_change_default_margin]
 
-  // documentation:start:13: Perform collision check
+  //! [collision_example_third_check]
   CONSOLE_BRIDGE_logInform("Test object inside the contact distance");
   result.clear();
   result_vector.clear();
 
-  // Check for contact with new threshold
   checker.contactTest(result, request);
   result.flattenMoveResults(result_vector);
 
@@ -170,5 +260,6 @@ int main(int /*argc*/, char** /*argv*/)
                            result_vector[0].link_names[0].c_str(),
                            result_vector[0].link_names[1].c_str(),
                            toString(result_vector[0].normal).c_str());
-  // documentation:end:13: Perform collision check
+  //! [collision_example_third_check]
 }
+//! [box_box_example_full_source]
