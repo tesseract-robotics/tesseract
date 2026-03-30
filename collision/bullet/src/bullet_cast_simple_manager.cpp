5aea96141b7c20e94a20469fe1739b86782114a5
Roelof Oomen <rj.oomen@isohorti.com>
2026-03-30 11:22:18 +0200

    Fix setCollisionObjectsTransform assuming an ordered map

--- collision/bullet/src/bullet_cast_simple_manager.cpp
+++ collision/bullet/src/bullet_cast_simple_manager.cpp
@@ -290,14 +290,11 @@ void BulletCastSimpleManager::setCollisionObjectsTransform(const tesseract::comm
                                                            const tesseract::common::TransformMap& pose2)
 {
   assert(pose1.size() == pose2.size());
-  auto it1 = pose1.begin();
-  auto it2 = pose2.begin();
-  while (it1 != pose1.end())
+  for (const auto& [name, tf1] : pose1)
   {
-    assert(pose1.find(it1->first) != pose2.end());
-    setCollisionObjectsTransform(it1->first, it1->second, it2->second);
-    std::advance(it1, 1);
-    std::advance(it2, 1);
+    auto it2 = pose2.find(name);
+    assert(it2 != pose2.end());
+    setCollisionObjectsTransform(name, tf1, it2->second);
   }
 }
 
